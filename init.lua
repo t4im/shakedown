56758130555cabf615fead26c5fe7a12b5f9fd41
Tim <t4im@users.noreply.github.com>
2015-02-04 17:40:20 +0100

    add test-entry log function

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
-mtt = { modpath = modpath }
+mtt = {
+	modpath = modpath,
+	entry = function(msg)
+		msg = string.format("running %s tests", msg)
+		minetest.log("action", msg)
+		minetest.chat_send_all(msg)
+	end,
+}
 package.path =
 	modpath .. "/?/init.lua;" ..
 	modpath .. "/?.lua;" ..
