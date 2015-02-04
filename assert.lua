c6a78bc39eefa773aff3c8d44d93ad022988813f
Tim <t4im@users.noreply.github.com>
2015-02-04 10:33:30 +0100

    integrate the assert calls into the minetest mod, run assertions with pcall and print them out in console and chat (for cooperative testing)

---
+++ assert.lua
@@ -0,0 +1,30 @@
+mtt.luassert = require("luassert")
+mtt.assert = {
+	print = function(msg)
+		minetest.log("error", msg)
+		minetest.chat_send_all(msg)
+	end
+}
+
+setmetatable(mtt.assert, {
+	__index = function(table, key)
+		local ref = mtt.luassert[key]
+		local ref_type = type(ref)
+		local ref_metatable = getmetatable(ref)
+		if ref_type == "function" or (ref_type == "table" and ref_metatable.__call) then
+			return function(...)
+				local result, err = pcall(ref, ...)
+				mtt.assert.print(err)
+				return result
+			end
+		elseif ref_type == "table" then
+			return ref
+		elseif ref_type == "nil" then
+			error("unknown assertion: " .. key)
+		end
+		error("unexpected datatype " .. tostring(type(ref)) .. " while looking for " .. key)
+	end,
+	__call = function(table, ...) table.True(...) end
+})
+
+mtt.assert(false)
--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
+mtt = { modpath = modpath }
+package.path =
+	modpath .. "/?/init.lua;" ..
+	modpath .. "/?.lua;" ..
+	package.path
+
+dofile(modpath .. "/assert.lua")
 dofile(modpath .. "/inspector.lua")
 
