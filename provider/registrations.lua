b262e9815df41a6a842f271ec22f8eea8ce94240
Tim <t4im@users.noreply.github.com>
2015-08-26 18:49:49 +0200

    add provider.craft_guide_items() as first provider making use of both the entries provider and the new matcher

--- provider/registrations.lua
+++ provider/registrations.lua
@@ -1,13 +1,6 @@
-local core = minetest
+local provider = mtt.provider
 
-function registered_items(max)
-	local registered_items = core.registered_items
-	local nested_iterator = pairs(registered_items)
-	return function()
-		local name, def = nested_iterator()
-		if not def then return nil end
-		local creative = (def.groups.not_in_creative_inventory and def.groups.not_in_creative_inventory ~= 0)
-				or not def.description or def.description ~= ""
-		return name, def, creative
-	end, registered_items, nil
+function provider.craft_guide_items()
+	return provider.entries(core.registered_items, nil, mtt.match.Not.in_creative_inventory())
 end
+
