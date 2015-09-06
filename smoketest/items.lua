60d4dc05a2953490d282594646bf969e698faed2
Tim <t4im@users.noreply.github.com>
2015-09-06 19:52:22 +0200

    clean up item smoketests a bit: fixing a few minor issues and abstracting pointed_thing's

--- smoketest/items.lua
+++ smoketest/items.lua
@@ -1,7 +1,33 @@
+local core, cubictest = core, cubictest
 local pos = { x=0, y=10, z=0 }
 local unknown_node_pos = { x=0, y=9, z=0 }
 local mock_player = cubictest.mocks.Player:new()
 local initial_stack_size = 5
+
+local pointed_at = {
+	an_unknown_node = {under=unknown_node_pos, above=pos, type="node"},
+	nothing = { type="nothing" },
+	itself = {under=pos, above={x=pos.x, y=pos.y+1, z=pos.z}, type="node"}
+}
+
+local function has_custom(def, name)
+	local field = def[name]
+	return field
+		and field ~= core.nodedef_default[name]
+		and field ~= core.craftitemdef_default[name]
+		and field ~= core.tooldef_default[name]
+		and field ~= core.noneitemdef_default[name]
+		and field ~= core.rotate_node
+end
+
+local node_specific_fields = {}
+for key, _ in pairs(core.nodedef_default) do
+	if not core.craftitemdef_default[key]
+		and not core.tooldef_default[key]
+		and not core.noneitemdef_default[key] then
+		table.insert(node_specific_fields, key)
+	end
+end
 
 for name, def in pairs(core.registered_items) do
 	local is_node = def.type == "node"
@@ -12,55 +38,47 @@ for name, def in pairs(core.registered_items) do
 			cubictest.provider.map_content(unknown_node_pos, cubictest.constants.CONTENT_UNKNOWN)
 		end)
 
-		if is_node
-			and def.on_place ~= core.nodedef_default.on_place
-			and def.on_place then
-			it("can be placed against an unknown node and will be removed from the ItemStack", function()
-				Given "an ItemStack()"
-				local stack = ItemStack { name = name, count = initial_stack_size }
-				And "a pointed_thing, pointing at an unknown node"
-				local pointed_thing = {under=unknown_node_pos, above=pos, type="node"}
+		if has_custom(def, "on_place") then
+			if is_node then
+				it("can be placed against an unknown node and will be removed from the ItemStack", function()
+					Given "an ItemStack()"
+					local stack = ItemStack { name = name, count = initial_stack_size }
+					And "a pointed_thing, pointing at an unknown node"
+					local pointed_thing = pointed_at.an_unknown_node
 
-				When "calling its on_place"
-				local left_over_stack = def.on_place(stack, mock_player, pointed_thing)
+					When "calling its on_place"
+					local left_over_stack = def.on_place(stack, mock_player, pointed_thing)
 
-				Then "place something"
-				-- This doesn't work well with e.g. expandable multinode objects.
-				-- Or anything else, that might abort the placement.
-				-- assert.is_not_equal("air", core.get_node(pos).name)
+					Then "place something"
+					-- This doesn't work well with e.g. expandable multinode objects.
+					-- Or anything else, that might abort the placement.
+					-- assert.is_not_equal("air", core.get_node(pos).name)
 
-				And "return the leftover itemstack"
-				assert.is_truthy(left_over_stack)
-				-- TODO better
+					And "return the leftover itemstack"
+					assert.is_truthy(left_over_stack)
+					-- TODO better
 
-				And "reduce the itemstack count"
-				assert.is_equal(initial_stack_size - 1, left_over_stack:get_count())
-			end)
-		elseif not is_node
-			and	def.on_place ~= core.craftitemdef_default.on_place
-			and def.on_place ~= core.tooldef_default.on_place
-			and def.on_place ~= core.noneitemdef_default.on_place
-			and def.on_place then
-			it("can be placed against an unknown node", function()
-				Given "an ItemStack()"
-				local stack = ItemStack { name = name, count = initial_stack_size }
-				And "a pointed_thing, pointing at an unknown node"
-				local pointed_thing = {under=unknown_node_pos, above=pos, type="node"}
+					And "reduce the itemstack count"
+					assert.is_equal(initial_stack_size - 1, left_over_stack:get_count())
+				end)
+			else
+				it("can be placed against an unknown node", function()
+					Given "an ItemStack()"
+					local stack = ItemStack { name = name, count = initial_stack_size }
+					And "a pointed_thing, pointing at an unknown node"
+					local pointed_thing = pointed_at.an_unknown_node
 
-				When "calling its on_place"
-				local left_over_stack = def.on_place(stack, mock_player, pointed_thing)
+					When "calling its on_place"
+					local left_over_stack = def.on_place(stack, mock_player, pointed_thing)
 
-				Then "return the leftover itemstack"
-				assert.is_truthy(left_over_stack)
-				-- TODO better
-			end)
+					Then "return the leftover itemstack"
+					assert.is_truthy(left_over_stack)
+					-- TODO better
+				end)
+			end
 		end
 
-		if def.on_drop ~= core.nodedef_default.on_drop
-			and def.on_drop ~= core.craftitemdef_default.on_drop
-			and def.on_drop ~= core.tooldef_default.on_drop
-			and def.on_drop ~= core.noneitemdef_default.on_drop
-			and def.on_drop then
+		if has_custom(def, "on_drop") then
 			it("can be dropped", function()
 				When "calling on_drop"
 				local left_over_stack = def.on_drop(ItemStack(name), mock_player, pos)
@@ -77,30 +95,21 @@ for name, def in pairs(core.registered_items) do
 		end
 
 		if def.on_use then
-			it("can be used pointing at unknown nodes", function()
-				Given "a pointed_thing, pointing at an unknown node"
-				local pointed_thing = {under=unknown_node_pos, above=pos, type="node"}
-				And "a player wielding the item"
-				mock_player:set_wielded_item(ItemStack(name))
+			for key, var in pairs(pointed_at) do
+				local target = key:gsub("_", " ")
+				it("can be used pointing at " .. target, function()
+					Given("a pointed_thing, pointing at " .. target)
+					local pointed_thing = var
+					And "a player wielding the item"
+					mock_player:set_wielded_item(ItemStack(name))
 
-				When "using against it"
-				local returned_stack = def.on_use(mock_player:get_wielded_item(), mock_player, pointed_thing)
+					When "using against it"
+					local returned_stack = def.on_use(mock_player:get_wielded_item(), mock_player, pointed_thing)
 
-				Then "return either an itemstack or nil"
-				-- TODO
-			end)
-			it("can be used pointing at nothing", function()
-				Given "a pointed_thing, pointing at nothing"
-				local pointed_thing = { type="nothing" }
-				And "a player wielding the item"
-				mock_player:set_wielded_item(ItemStack(name))
-
-				When "using against it"
-				local returned_stack = def.on_use(mock_player:get_wielded_item(), mock_player, pointed_thing)
-
-				Then "return either an itemstack or nil"
-				-- TODO
-			end)
+					Then "return either an itemstack or nil"
+					-- TODO
+				end)
+			end
 		end
 
 		if def.after_use then
@@ -114,16 +123,12 @@ for name, def in pairs(core.registered_items) do
 			end)
 		end
 
-		if not is_node then
-			local node_specific_fields = {}
-			for key, _ in pairs(core.nodedef_default) do
-				if not core.craftitemdef_default[key]
-					and not core.tooldef_default[key]
-					and not core.noneitemdef_default[key] then
-					table.insert(node_specific_fields, key)
-				end
-			end
+		it("does not use any deprecated fields", function()
+			assert.is.Nil(def.tile_images) -- tiles
+			assert.is.Nil(def.special_materials) -- special_tiles
+		end)
 
+		if not is_node then
 			it("does not register any node specific functions without being a node", function()
 				local no = assert.is_nil
 				for _, field in ipairs(node_specific_fields) do
@@ -133,14 +138,9 @@ for name, def in pairs(core.registered_items) do
 		end
 
 		if is_node then
-			it("doesn't use any deprecated fields", function()
-				assert.is.Nil(def.tile_images) -- tiles
-				assert.is.Nil(def.special_materials) -- special_tiles
-			end)
-
 			if def.on_punch ~= core.nodedef_default.on_punch and def.on_punch then
 				it("can be punched by a player", function()
-					def.on_punch(pos, core.get_node(pos), mock_player, {under=pos, above={x=pos.x, y=pos.y+1, z=pos.z}, type="node"})
+					def.on_punch(pos, core.get_node(pos), mock_player, pointed_at.itself)
 				end)
 				it("can be punched by core.punch_node(pos) (nil player)", function()
 					core.punch_node(pos)
@@ -154,29 +154,29 @@ for name, def in pairs(core.registered_items) do
 					local stack = ItemStack("default:stone 1")
 
 					When "right clicked"
-					def.on_right_click(pos, core.get_node(pos), mock_player, stack)
+					def.on_right_click(pos, core.get_node(pos), mock_player, stack, nil)
 
 					Then "return the leftover itemstack"
-					assert.is_truthy(left_over_stack)
+					assert.is_truthy(stack)
 					-- TODO better
 
 				end)
 				it("can be rightclicked by an empty handed player", function()
 					Given "a pointed_thing"
-					local pointed_thing = {under=pos, above={x=pos.x, y=pos.y+1, z=pos.z}, type="node"}
+					local pointed_thing = pointed_at.itself
 					And "an empty ItemStack()"
-					local stack = ItemStack("default:stone 1")
+					local stack = ItemStack()
 
 					When "right clicked"
 					def.on_right_click(pos, core.get_node(pos), mock_player, stack, pointed_thing)
 
 					Then "return the leftover itemstack"
-					assert.is_truthy(left_over_stack)
+					assert.is_truthy(stack)
 					-- TODO better
 				end)
 				it("can be rightclicked by an undefined itemstack", function()
 					Given "a pointed_thing"
-					local pointed_thing = {under=pos, above={x=pos.x, y=pos.y+1, z=pos.z}, type="node"}
+					local pointed_thing = pointed_at.itself
 					And "an undefined ItemStack()"
 					-- from the api: _if defined_, itemstack will hold clicker's wielded item
 					local stack = nil
@@ -185,13 +185,13 @@ for name, def in pairs(core.registered_items) do
 					def.on_right_click(pos, core.get_node(pos), mock_player, stack, pointed_thing)
 
 					Then "return the leftover itemstack"
-					assert.is_truthy(left_over_stack)
+					assert.is_truthy(stack)
 					-- TODO better
 				end)
 			end
 
 			if def.on_receive_fields then
-				it("can receive an emtpy formspec response", function()
+				it("can receive an empty formspec response", function()
 					def.on_receive_fields(pos, name, {}, mock_player)
 				end)
 			end
