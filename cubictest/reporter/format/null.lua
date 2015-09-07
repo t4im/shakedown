4cf3d67b96be2da826fd73cb385ef7a6834c30e2
Tim <t4im@users.noreply.github.com>
2015-09-07 13:45:32 +0200

    add a null-format (as template and /dev/null kind of formatter)

---
+++ cubictest/reporter/format/null.lua
@@ -0,0 +1,29 @@
+local string = string
+return cubictest.formatter:new {
+	["Suite Start"] = function(self, run, event)
+	end,
+
+	["Specification Start"] = function(self, run, event)
+	end,
+
+	["TestCase Start"] = function(self, run, event)
+	end,
+
+	["Step"] = function(self, run, event)
+	end,
+
+	["TestCase End"] = function(self, run, event)
+	end,
+
+	["Specification End"] = function(self, run, event)
+	end,
+
+	["Specification Error"] = function(self, run, event)
+	end,
+
+	["Suite End"] = function(self, run, event)
+	end,
+
+	["Generic Error"] = function(self, run, event)
+	end,
+}
