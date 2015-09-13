70807456a1b1ad8c5aa2142fab4487f98580e194
Tim <t4im@users.noreply.github.com>
2015-09-13 22:31:09 +0200

    remove dependency on symlinks from cubictest

--- cubictest/lib/init.lua
+++ cubictest/lib/init.lua
@@ -4,7 +4,18 @@ local modpath = cubictest.modpath
 package.path =
 	modpath .. "/?/init.lua;" ..
 	modpath .. "/?.lua;" ..
+	modpath .. "/lib/?/src/init.lua;" ..
+	modpath .. "/lib/luassert/src/?.lua;" ..
+	modpath .. "/lib/luassert/src/?/init.lua;" ..
 	package.path
+
+table.insert(package.loaders, function(name)
+	--  luassert internally calls luassert.<module>, which won't resolve with the src directory in the submodule, so we strip it off
+	local smod = name:match("luassert%.(.*)")
+	if not smod then return end
+	-- TODO this should not assume an order despite being declared in the api:
+	return package.loaders[2](smod)
+end)
 
 cubictest.say = require("say")
 cubictest.assert = require("luassert")
