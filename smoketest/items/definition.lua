f91f5d6ea652650da5a7903a815f8bb23ba79ae3
Tim <t4im@users.noreply.github.com>
2016-07-01 22:21:41 +0200

    smoketest for identical item definitions between items

--- smoketest/items/definition.lua
+++ smoketest/items/definition.lua
@@ -1,3 +1,5 @@
+local format = string.format
+
 local node_specific_fields = {}
 for key, _ in pairs(core.nodedef_default) do
 	if not core.craftitemdef_default[key]
@@ -7,21 +9,31 @@ for key, _ in pairs(core.nodedef_default) do
 	end
 end
 
+local no = assert.is.Nil
+local are_not_equal = assert.are_not_equal
+
 return function(name, def)
 	local is_node = def.type == "node"
 
 	describe(name .. " definition table", function()
 		it("does not use any deprecated fields", function()
-			assert.is.Nil(def.tile_images) -- tiles
-			assert.is.Nil(def.special_materials) -- special_tiles
+			no(def.tile_images) -- tiles
+			no(def.special_materials) -- special_tiles
 		end)
 		if not is_node then
 			it("does not register any node specific functions without being a node", function()
-				local no = assert.is_nil
 				for _, field in ipairs(node_specific_fields) do
 					no(def[field])
 				end
 			end)
 		end
+		--TODO ~1s (5s before localization) loop, perhaps we avoid back and forth testing later?
+		it("does not share its definition table with another item", function()
+			for item_name, item_def in pairs(core.registered_items) do
+				if item_name ~= name then -- skip us self
+					are_not_equal(item_def, def, format("Item %s has the same table as %s", item_name, name))
+				end
+			end
+		end)
 	end)
 end
