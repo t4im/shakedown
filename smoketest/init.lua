b78fd76390d76db4a5e799c6e3eca234b60b1394
Tim <t4im@users.noreply.github.com>
2016-07-30 18:42:55 +0200

    have core.get_player_by_name find Sam

--- smoketest/init.lua
+++ smoketest/init.lua
@@ -4,6 +4,16 @@ smoketest = {
 	expect_infinite_stacks = core.setting_getbool("creative_mode"),
 	sam = cubictest.dummies.Player:new()
 }
+local sam = smoketest.sam
+
+local orig_get_player_by_name = core.get_player_by_name
+core.get_player_by_name = function(name)
+	if name == sam:get_player_name() then
+		return sam
+	end
+	return orig_get_player_by_name(name)
+end
+
 dofile(modpath .. "/testbox/init.lua")
 
 dofile(modpath .. "/logusage.lua")
